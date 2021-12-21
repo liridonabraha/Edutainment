30c6233e93febd2be346ecaa587613b962d5e819
Dom Bryan <dombguitar@gmail.com>
2021-12-21 18:46:13 +0000

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Dom Bryan on 21/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
